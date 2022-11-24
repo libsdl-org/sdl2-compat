85bbefdcfb48e5a719402d91c6585a8a1973d548
Ryan C. Gordon <icculus@icculus.org>
2022-11-24 01:04:42 -0500

    Added initial shot at sdl2-compat framework.
    Just loads stuff and knows what symbols to passthrough. As ABI breaks for
    SDL3 we will add more to this.
    
    Untested!

---
+++ src/sdl2_compat.c
@@ -0,0 +1,515 @@
+/*
+  Simple DirectMedia Layer
+  Copyright (C) 1997-2022 Sam Lantinga <slouken@libsdl.org>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+/* This file contains functions for backwards compatibility with SDL2 */
+
+#include "sdl3_include_wrapper.h"
+
+/*
+ * We report the library version as
+ * 2.$(SDL2_COMPAT_VERSION_MINOR).$(SDL2_COMPAT_VERSION_PATCH). This number
+ * should be way ahead of what SDL2 Classic would report, so apps can
+ * decide if they're running under the compat layer, if they really care.
+ * The patch level changes in release cycles. The minor version starts at 90
+ * to be high by default, and usually doesn't change (and maybe never changes).
+ * The number might increment past 90 if there are a ton of releases.
+ */
+#define SDL2_COMPAT_VERSION_MINOR 90
+#define SDL2_COMPAT_VERSION_PATCH 0
+
+#include <stdarg.h>
+#include <limits.h>
+#include <stddef.h>
+#if defined(_MSC_VER) && (_MSC_VER < 1600)
+/* intptr_t already handled by stddef.h. */
+#else
+#include <stdint.h>
+#endif
+
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN 1
+#endif
+#include <windows.h>
+#else
+#include <stdio.h> /* fprintf(), etc. */
+#include <stdlib.h>    /* for abort() */
+#include <string.h>
+#endif
+
+/* mingw headers may define these ... */
+#undef strtod
+#undef strcasecmp
+#undef strncasecmp
+#undef snprintf
+#undef vsnprintf
+
+#define SDL_BlitSurface SDL_UpperBlit
+
+#ifdef __linux__
+#include <unistd.h> /* for readlink() */
+#endif
+
+#if defined(__unix__) || defined(__APPLE__)
+#ifndef PATH_MAX
+#define PATH_MAX 1024
+#endif
+#define SDL12_MAXPATH PATH_MAX
+#elif defined _WIN32
+#define SDL12_MAXPATH MAX_PATH
+#elif defined __OS2__
+#define SDL12_MAXPATH CCHMAXPATH
+#else
+#define SDL12_MAXPATH 1024
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SDL3_SYM(rc,fn,params,args,ret) \
+    typedef rc (SDLCALL *SDL3_##fn##_t) params; \
+    static SDL3_##fn##_t SDL3_##fn = NULL;
+#include "sdl3_syms.h"
+
+/* Things that _should_ be binary compatible pass right through... */
+#define SDL3_SYM_PASSTHROUGH(rc,fn,params,args,ret) \
+    DECLSPEC rc SDLCALL SDL_##fn params { ret SDL3_##fn args; }
+#include "sdl3_syms.h"
+
+
+/* these are macros (etc) in the SDL headers, so make our own. */
+#define SDL3_OutOfMemory() SDL3_Error(SDL_ENOMEM)
+#define SDL3_Unsupported() SDL3_Error(SDL_UNSUPPORTED)
+#define SDL3_InvalidParamError(param) SDL3_SetError("Parameter '%s' is invalid", (param))
+#define SDL3_zero(x) SDL3_memset(&(x), 0, sizeof((x)))
+#define SDL3_zerop(x) SDL3_memset((x), 0, sizeof(*(x)))
+#define SDL_ReportAssertion SDL3_ReportAssertion
+
+/* Obviously we can't use SDL_LoadObject() to load SDL2.  :)  */
+static char loaderror[256];
+#if defined(_WIN32)
+    #define DIRSEP "\\"
+    #define SDL3_LIBNAME "SDL3.dll"
+    static HMODULE Loaded_SDL3 = NULL;
+    #define LoadSDL3Library() ((Loaded_SDL3 = LoadLibraryA(SDL3_LIBNAME)) != NULL)
+    #define LookupSDL3Sym(sym) (void *)GetProcAddress(Loaded_SDL3, sym)
+    #define CloseSDL3Library() { if (Loaded_SDL3) { FreeLibrary(Loaded_SDL3); Loaded_SDL3 = NULL; } }
+    #define strcpy_fn  lstrcpyA
+    #define sprintf_fn wsprintfA
+#elif defined(__APPLE__)
+    #include <dlfcn.h>
+    #include <pwd.h>
+    #include <unistd.h>
+    #define SDL3_LIBNAME "libSDL3-3.0.0.dylib"
+    /* SDL2 cmake'ry is (was?) messy: */
+    #define SDL3_LIBNAME2 "libSDL3-3.0.dylib"
+    #define SDL3_FRAMEWORK "SDL3.framework/Versions/A/SDL3"
+    #define strcpy_fn  strcpy
+    #define sprintf_fn sprintf
+    static void *Loaded_SDL3 = NULL;
+    #define LookupSDL3Sym(sym) dlsym(Loaded_SDL3, sym)
+    #define CloseSDL3Library() { if (Loaded_SDL3) { dlclose(Loaded_SDL3); Loaded_SDL3 = NULL; } }
+    static SDL_bool LoadSDL3Library(void) {
+        /* I don't know if this is the _right_ order to try, but this seems reasonable */
+        static const char * const dylib_locations[] = {
+            "@loader_path/" SDL3_LIBNAME, /* MyApp.app/Contents/MacOS/libSDL2-2.0.0.dylib */
+            "@loader_path/" SDL3_LIBNAME2, /* MyApp.app/Contents/MacOS/libSDL2-2.0.dylib */
+            "@loader_path/../Frameworks/" SDL3_FRAMEWORK, /* MyApp.app/Contents/Frameworks/SDL2.framework */
+            "@executable_path/" SDL3_LIBNAME, /* MyApp.app/Contents/MacOS/libSDL2-2.0.0.dylib */
+            "@executable_path/" SDL3_LIBNAME2, /* MyApp.app/Contents/MacOS/libSDL2-2.0.dylib */
+            "@executable_path/../Frameworks/" SDL3_FRAMEWORK, /* MyApp.app/Contents/Frameworks/SDL2.framework */
+            NULL,  /* /Users/username/Library/Frameworks/SDL2.framework */
+            "/Library/Frameworks" SDL3_FRAMEWORK, /* /Library/Frameworks/SDL2.framework */
+            SDL3_LIBNAME, /* oh well, anywhere the system can see the .dylib (/usr/local/lib or whatever) */
+            SDL3_LIBNAME2
+        };
+
+        int i;
+        for (i = 0; i < (int) SDL_arraysize(dylib_locations); i++) {
+            const char *location = dylib_locations[i];
+            if (location) {
+                Loaded_SDL3 = dlopen(location, RTLD_LOCAL|RTLD_NOW);
+            } else { /* hack to mean "try homedir" */
+                const char *homedir = NULL;
+                struct passwd *pwent = getpwuid(getuid());
+                if (pwent) {
+                    homedir = pwent->pw_dir;
+                }
+                if (!homedir) {
+                    homedir = getenv("HOME");
+                }
+                if (homedir) {
+                    char framework[512];
+                    const int rc = snprintf(framework, sizeof (framework), "%s/Library/Frameworks/" SDL3_FRAMEWORK, homedir);
+                    if ((rc > 0) && (rc < (int) sizeof(framework))) {
+                        Loaded_SDL3 = dlopen(framework, RTLD_LOCAL|RTLD_NOW);
+                    }
+                }
+            }
+
+            if (Loaded_SDL3) {
+                return SDL_TRUE;
+            }
+        }
+
+        return SDL_FALSE; /* didn't find it anywhere reasonable. :( */
+    }
+#elif defined(__unix__)
+    #include <dlfcn.h>
+    #define SDL3_LIBNAME "libSDL2-2.0.so.0"
+    static void *Loaded_SDL3 = NULL;
+    #define LoadSDL3Library() ((Loaded_SDL3 = dlopen(SDL3_LIBNAME, RTLD_LOCAL|RTLD_NOW)) != NULL)
+    #define LookupSDL3Sym(sym) dlsym(Loaded_SDL3, sym)
+    #define CloseSDL3Library() { if (Loaded_SDL3) { dlclose(Loaded_SDL3); Loaded_SDL3 = NULL; } }
+    #define strcpy_fn  strcpy
+    #define sprintf_fn sprintf
+#else
+    #error Please define your platform.
+#endif
+
+#ifndef SDL3_REQUIRED_VER
+#define SDL3_REQUIRED_VER SDL_VERSIONNUM(3,0,0)
+#endif
+
+#ifndef DIRSEP
+#define DIRSEP "/"
+#endif
+
+static void *
+LoadSDL3Symbol(const char *fn, int *okay)
+{
+    void *retval = NULL;
+    if (*okay) { /* only bother trying if we haven't previously failed. */
+        retval = LookupSDL3Sym(fn);
+        if (retval == NULL) {
+            sprintf_fn(loaderror, "%s missing in SDL2 library.", fn);
+            *okay = 0;
+        }
+    }
+    return retval;
+}
+
+static void
+UnloadSDL3(void)
+{
+    #define SDL3_SYM(rc,fn,params,args,ret) SDL3_##fn = NULL;
+    #include "sdl3_syms.h"
+    CloseSDL3Library();
+}
+
+typedef struct QuirkEntryType
+{
+    const char *exe_name;
+    const char *hint_name;
+    const char *hint_value;
+} QuirkEntryType;
+
+static QuirkEntryType quirks[] = {
+    /* TODO: Add any quirks needed for various systems. */
+    {"", "", "0"} /* A dummy entry to keep compilers happy. */
+};
+
+#ifdef __linux__
+static void OS_GetExeName(char *buf, const unsigned maxpath) {
+    buf[0] = '\0';
+    readlink("/proc/self/exe", buf, maxpath);
+}
+#elif defined(_WIN32)
+static void OS_GetExeName(char *buf, const unsigned maxpath) {
+    buf[0] = '\0';
+    GetModuleFileNameA(NULL, buf, maxpath);
+}
+#elif defined(__APPLE__) || defined(__FREEBSD__)
+static void OS_GetExeName(char *buf, const unsigned maxpath) {
+    const char *progname = getprogname();
+    if (progname != NULL) {
+        strlcpy(buf, progname, maxpath);
+    } else {
+        buf[0] = '\0';
+    }
+}
+#else
+#warning Please implement this for your platform.
+static void OS_GetExeName(char *buf, const unsigned maxpath) {
+    buf[0] = '\0';
+    (void)maxpath;
+}
+#endif
+
+static const char *
+SDL2Compat_GetExeName(void)
+{
+    static const char *exename = NULL;
+    if (exename == NULL) {
+        static char path_buf[SDL12_MAXPATH];
+        static char *base_path;
+        OS_GetExeName(path_buf, SDL12_MAXPATH);
+        base_path = SDL3_strrchr(path_buf, *DIRSEP);
+        if (base_path) {
+            /* We have a '\\' component. */
+            exename = base_path + 1;
+        } else {
+            /* No slashes, return the whole module filanem. */
+            exename = path_buf;
+        }
+    }
+    return exename;
+}
+
+static const char *
+SDL2Compat_GetHint(const char *name)
+{
+    return SDL3_getenv(name);
+}
+
+static SDL_bool
+SDL2Compat_GetHintBoolean(const char *name, SDL_bool default_value)
+{
+    const char *val = SDL2Compat_GetHint(name);
+
+    if (!val) {
+        return default_value;
+    }
+
+    return (SDL3_atoi(val) != 0) ? SDL_TRUE : SDL_FALSE;
+}
+
+static float
+SDL2Compat_GetHintFloat(const char *name, float default_value)
+{
+    const char *val = SDL2Compat_GetHint(name);
+
+    if (!val) {
+        return default_value;
+    }
+
+    return (float) SDL3_atof(val);
+}
+
+static void
+SDL2Compat_ApplyQuirks(SDL_bool force_x11)
+{
+    const char *exe_name = SDL2Compat_GetExeName();
+    int i;
+
+    if (WantDebugLogging) {
+        SDL3_Log("This app appears to be named '%s'", exe_name);
+    }
+
+    #ifdef __linux__
+    if (force_x11) {
+        const char *videodriver_env = SDL3_getenv("SDL_VIDEODRIVER");
+        if (videodriver_env && (SDL3_strcmp(videodriver_env, "x11") != 0)) {
+            if (WantDebugLogging) {
+                SDL3_Log("This app looks like it requires X11, but the SDL_VIDEODRIVER environment variable is set to \"%s\". If you have issues, try setting SDL_VIDEODRIVER=x11", videodriver_env);
+            }
+        } else {
+            if (WantDebugLogging) {
+                SDL3_Log("sdl12-compat: We are forcing this app to use X11, because it probably talks to an X server directly, outside of SDL. If possible, this app should be fixed, to be compatible with Wayland, etc.");
+            }
+            SDL3_setenv("SDL_VIDEODRIVER", "x11", 1);
+        }
+    }
+    #endif
+
+    if (*exe_name == '\0') {
+        return;
+    }
+    for (i = 0; i < (int) SDL_arraysize(quirks); i++) {
+        if (!SDL3_strcmp(exe_name, quirks[i].exe_name)) {
+            if (!SDL3_getenv(quirks[i].hint_name)) {
+                if (WantDebugLogging) {
+                    SDL3_Log("Applying compatibility quirk %s=\"%s\" for \"%s\"", quirks[i].hint_name, quirks[i].hint_value, exe_name);
+                }
+                SDL3_setenv(quirks[i].hint_name, quirks[i].hint_value, 1);
+            } else {
+                if (WantDebugLogging) {
+                    SDL3_Log("Not applying compatibility quirk %s=\"%s\" for \"%s\" due to environment variable override (\"%s\")\n",
+                            quirks[i].hint_name, quirks[i].hint_value, exe_name, SDL3_getenv(quirks[i].hint_name));
+                }
+            }
+        }
+    }
+}
+
+static int
+LoadSDL3(void)
+{
+    int okay = 1;
+    if (!Loaded_SDL3) {
+        SDL_bool force_x11 = SDL_FALSE;
+
+        #ifdef __linux__
+        void *global_symbols = dlopen(NULL, RTLD_LOCAL|RTLD_NOW);
+
+        /* Use linked libraries to detect what quirks we are likely to need */
+        if (global_symbols != NULL) {
+            if (dlsym(global_symbols, "glxewInit") != NULL) {  /* GLEW (e.g. Frogatto, SLUDGE) */
+                force_x11 = SDL_TRUE;
+            } else if (dlsym(global_symbols, "cgGLEnableProgramProfiles") != NULL) {  /* NVIDIA Cg (e.g. Awesomenauts, Braid) */
+                force_x11 = SDL_TRUE;
+            } else if (dlsym(global_symbols, "_Z7ssgInitv") != NULL) {  /* ::ssgInit(void) in plib (e.g. crrcsim) */
+                force_x11 = SDL_TRUE;
+            }
+            dlclose(global_symbols);
+        }
+        #endif
+
+        okay = LoadSDL3Library();
+        if (!okay) {
+            strcpy_fn(loaderror, "Failed loading SDL3 library.");
+        } else {
+            #define SDL3_SYM(rc,fn,params,args,ret) SDL3_##fn = (SDL3_##fn##_t) LoadSDL3Symbol("SDL_" #fn, &okay);
+            #include "sdl3_syms.h"
+            if (okay) {
+                SDL_version v;
+                SDL3_GetVersion(&v);
+                LinkedSDL2VersionInt = SDL_VERSIONNUM(v.major, v.minor, v.patch);
+                okay = (LinkedSDL2VersionInt >= SDL3_REQUIRED_VER);
+                if (!okay) {
+                    sprintf_fn(loaderror, "SDL3 %d.%d.%d library is too old.", v.major, v.minor, v.patch);
+                } else {
+                    WantDebugLogging = SDL2Compat_GetHintBoolean("SDL12COMPAT_DEBUG_LOGGING", SDL_FALSE);
+                    if (WantDebugLogging) {
+                        #if defined(__DATE__) && defined(__TIME__)
+                        SDL3_Log("sdl2-compat 2.%d.%d, built on " __DATE__ " at " __TIME__ ", talking to SDL3 %d.%d.%d", SDL2_COMPAT_VERSION_MINOR, SDL2_COMPAT_VERSION_PATCH, v.major, v.minor, v.patch);
+                        #else
+                        SDL3_Log("sdl2-compat 2.%d.%d, talking to SDL3 %d.%d.%d", SDL2_COMPAT_VERSION_MINOR, SDL2_COMPAT_VERSION_PATCH, v.major, v.minor, v.patch);
+                        #endif
+                    }
+                    SDL2Compat_ApplyQuirks(force_x11);  /* Apply and maybe print a list of any enabled quirks. */
+                }
+            }
+            if (!okay) {
+                UnloadSDL3();
+            }
+        }
+    }
+    return okay;
+}
+
+#if defined(_WIN32)
+static void error_dialog(const char *errorMsg)
+{
+    MessageBoxA(NULL, errorMsg, "Error", MB_OK | MB_SETFOREGROUND | MB_ICONSTOP);
+}
+#elif defined(__APPLE__)
+extern void error_dialog(const char *errorMsg);
+#else
+static void error_dialog(const char *errorMsg)
+{
+    fprintf(stderr, "%s\n", errorMsg);
+}
+#endif
+
+#if defined(__GNUC__) && !defined(_WIN32)
+static void dllinit(void) __attribute__((constructor));
+static void dllinit(void)
+{
+    if (!LoadSDL3()) {
+        error_dialog(loaderror);
+        abort();
+    }
+}
+static void dllquit(void) __attribute__((destructor));
+static void dllquit(void)
+{
+    UnloadSDL3();
+}
+
+#elif defined(_WIN32) && (defined(_MSC_VER) || defined(__MINGW32__) || defined(__WATCOMC__))
+#if defined(_MSC_VER) && !defined(__FLTUSED__)
+#define __FLTUSED__
+__declspec(selectany) int _fltused = 1;
+#endif
+#if defined(__MINGW32__)
+#define _DllMainCRTStartup DllMainCRTStartup
+#endif
+#if defined(__WATCOMC__)
+#define _DllMainCRTStartup LibMain
+#endif
+BOOL WINAPI _DllMainCRTStartup(HANDLE dllhandle, DWORD reason, LPVOID reserved)
+{
+    (void) dllhandle;
+    (void) reserved;
+    switch (reason) {
+    case DLL_PROCESS_DETACH:
+        UnloadSDL3();
+        break;
+
+    case DLL_PROCESS_ATTACH: /* init once for each new process */
+        if (!LoadSDL3()) {
+            error_dialog(loaderror);
+            #if 0
+            TerminateProcess(GetCurrentProcess(), 42);
+            ExitProcess(42);
+            #endif
+            return FALSE;
+        }
+        break;
+
+    case DLL_THREAD_ATTACH: /* thread-specific init. */
+    case DLL_THREAD_DETACH: /* thread-specific cleanup */
+        break;
+    }
+    return TRUE;
+}
+
+#else
+    #error Please define an init procedure for your platform.
+#endif
+
+
+DECLSPEC const SDL_version * SDLCALL
+SDL_Linked_Version(void)
+{
+    static const SDL_version version = { 2, SDL2_COMPAT_VERSION_MINOR, SDL2_COMPAT_VERSION_PATCH };
+    return &version;
+}
+
+DECLSPEC int SDLCALL
+SDL_sscanf(const char *text, const char *fmt, ...)
+{
+    int retval;
+    va_list ap;
+    va_start(ap, fmt);
+    retval = (int) SDL3_vsscanf(text, fmt, ap);
+    va_end(ap);
+    return retval;
+}
+
+DECLSPEC int SDLCALL
+SDL_snprintf(char *text, size_t maxlen, const char *fmt, ...)
+{
+    int retval;
+    va_list ap;
+    va_start(ap, fmt);
+    retval = (int) SDL3_vsnprintf(text, maxlen, fmt, ap);
+    va_end(ap);
+    return retval;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+/* vi: set ts=4 sw=4 expandtab: */
