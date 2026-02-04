7e7975e59c5f1a602724b560940f44fa2a9967a4
Sam Lantinga <slouken@libsdl.org>
2026-02-04 09:59:04 -0800

    Updated to version 2.32.65 for development

--- include/SDL2/SDL_version.h
+++ include/SDL2/SDL_version.h
@@ -58,7 +58,7 @@ typedef struct SDL_version
 */
 #define SDL_MAJOR_VERSION   2
 #define SDL_MINOR_VERSION   32
-#define SDL_PATCHLEVEL      64
+#define SDL_PATCHLEVEL      65
 
 /**
  * Macro to determine SDL version program was compiled against.
