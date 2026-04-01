3c0b0f4b85c60b69c694ebb140e72395f51a77e4
Sam Lantinga <slouken@libsdl.org>
2026-04-01 16:42:14 -0700

    Fixed crash if SDL_LoadBMP_RW() fails (thanks @smcv!)

--- src/sdl2_compat.c
+++ src/sdl2_compat.c
@@ -4069,7 +4069,7 @@ SDL_LoadBMP_RW(SDL2_RWops *rwops2, int freesrc)
         SDL_RWclose(rwops2);
     }
 
-    if (SDL_BITSPERPIXEL(retval->format) < 8) {  /* SDL3 can provide BMPs that are < 8bpp. In SDL2, these would get converted to 8bpp. */
+    if (retval && SDL_BITSPERPIXEL(retval->format) < 8) {  /* SDL3 can provide BMPs that are < 8bpp. In SDL2, these would get converted to 8bpp. */
         SDL_Surface *cvt = SDL3_ConvertSurfaceAndColorspace(retval, SDL_PIXELFORMAT_INDEX8, SDL3_GetSurfacePalette(retval), SDL3_GetSurfaceColorspace(retval), 0);
         SDL3_DestroySurface(retval);
         retval = cvt;  /* if conversion failed, Surface3to2(), below, will notice. */
